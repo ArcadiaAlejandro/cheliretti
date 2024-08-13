70b7017e720837c7ce6f1c165a24df777eef0cf2
Alejandro Antonio <alejandrodelacruzcharun@gmail.com>
2024-08-13 12:03:48 -0500

    se ha agregado la url de cheliretti

--- vite.config.ts
+++ vite.config.ts
@@ -4,5 +4,5 @@ import react from '@vitejs/plugin-react-swc'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: "https://cheliretti.com"
+  base: "https://www.cheliretti.com/"
 })
